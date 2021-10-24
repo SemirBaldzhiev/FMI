55cbd8fb058162f11b72f9c773d9b3add03d637e
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-10-24 22:50:25 +0300

    Added task 3 from seminar

---
+++ Programming-Basics/Week-03-Exercises/Task3-Seminar/Task3-Seminar.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int firstNum;
+	int secondNum;
+	char symbol;
+
+	int result = 0;
+
+	std::cin >> firstNum >> secondNum >> symbol;
+
+	switch (symbol)
+	{
+		case '+':
+			result = firstNum + secondNum;
+			break;
+		case '-':
+			result = firstNum - secondNum;
+			break;
+		case '*':
+			result = firstNum * secondNum;
+			break;
+		case '/':
+			result = firstNum / secondNum;
+			break;
+		case '%':
+			result = firstNum % secondNum;
+			break;
+		case '^':
+			result = std::pow(firstNum, secondNum);
+			break;
+		case '=':
+			result = (firstNum == secondNum);
+			break;
+
+	}
+
+	if (symbol == '=')
+	{
+		std::cout << std::boolalpha << (bool)result << std::endl;
+	}
+	else 
+	{
+		std::cout << result << std::endl;
+	}
+
+	return 0;
+}
+
