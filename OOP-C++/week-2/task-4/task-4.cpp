88ed1b26b87886e656600ba69bb2c4952db812e3
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-02 18:22:00 +0200

    Finish task 4

--- OOP-C++/week-2/task-4/task-4.cpp
+++ OOP-C++/week-2/task-4/task-4.cpp
@@ -1,14 +1,101 @@
 #include <iostream>
 
 struct StudentBook {
-    char* studentName;
-    char* fn;
-    size_t countGrades;
-    double* grades;
+    char studentName[32];
+    char fn[16];
+    unsigned int countGrades;
+    double grades[16];
 };
 
-void AddGrade() {
+void AddGrade(StudentBook& sb, double grade) {
 
+    if (sb.countGrades < 16) {
+        sb.grades[sb.countGrades] = grade;
+        sb.countGrades++;
+    }
+}
+
+void swap(double* a, double* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sortGradesDesc(StudentBook& sb) {
+
+    int count = sb.countGrades;
+
+    for (int i = 0; i < count - 1; i++) {
+
+        for (int j = 0; j < count - i - 1; j++) {
+            if (sb.grades[i] > sb.grades[j]) {
+                swap(&sb.grades[i], &sb.grades[j]);
+            }
+        }
+    }
+}
+
+double avgGrades(const StudentBook& sb) {
+
+    double sum = 0;
+    int countCompletedExams = 0;
+
+    for (int i = 0; i < sb.countGrades; i++) {
+        if (sb.grades[i] >= 3.00) {
+            sum += sb.grades[i];
+            countCompletedExams++;
+        }
+    }
+
+    return sum / countCompletedExams;
+}
+
+bool canContinueHigherCourse(const StudentBook& sb) {
+
+    int notCompletedExams = 0;
+    bool canContinue = true;
+
+    for (int i = 0; i < sb.countGrades; i++)
+    {
+        if (sb.grades[i] >= 2.00 && sb.grades[i] < 3.00) {
+            notCompletedExams++;
+        }
+    }
+
+    if (notCompletedExams < 4){
+        canContinue = true;
+    }
+
+    if (notCompletedExams > 4) {
+        canContinue = false;
+    }
+
+    if (notCompletedExams == 4) {
+        if (avgGrades(sb) >= 4.00) {
+            canContinue = true;
+        }
+        else {
+            canContinue = false;
+        }
+    }
+
+    return canContinue;
+}
+
+bool canTakeScholarship(const StudentBook& sb) {
+    
+    for (int i = 0; i < sb.countGrades; i++) {
+        if (sb.grades[i] >= 2.00 && sb.grades[i] < 3.00) {
+            return false;
+        }
+    }
+    
+    if (avgGrades(sb) >= 5.00) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 int main()
