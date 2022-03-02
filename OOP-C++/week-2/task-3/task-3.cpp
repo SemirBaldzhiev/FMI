d0302df1c46e94fec8a299edfe710ff548bae5cf
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-02 17:36:09 +0200

    Finish task 3

--- OOP-C++/week-2/task-3/task-3.cpp
+++ OOP-C++/week-2/task-3/task-3.cpp
@@ -27,18 +27,58 @@ void initialize(Hero& hero) {
     hero.name = new char[nameSize];
 
     std::cin >> hero.name;
-    //std::cin >> hero.division;
+    
+    char divisionInput[8];
+
+    std::cin >> divisionInput;
+
+    if (strcmp(divisionInput, "Assasin") == 0) {
+        hero.division = ASSASSIN;
+    }
+    else if (strcmp(divisionInput, "Medic") == 0) {
+        hero.division = MEDIC;
+    }
+    else if (strcmp(divisionInput, "Mage") == 0) {
+        hero.division = MAGE;
+    }
+    else if (strcmp(divisionInput, "Tank") == 0) {
+        hero.division = TANK;
+    }
+    
+    char combatInput[8];
+    std::cin >> combatInput;
+
+    if (strcmp(divisionInput, "Range") == 0) {
+        hero.combat = RANGE;
+    }
+    else if (strcmp(divisionInput, "Mele") == 0) {
+        hero.combat = MELEE;
+    }
+    else if (strcmp(divisionInput, "Both") == 0) {
+        hero.combat = BOTH;
+    }
 }
 
 void printHero(const Hero& hero) {
     std::cout << "Name: " << hero.name << std::endl;
 
+    std::cout << "Division: ";
+
     switch (hero.division)
     {
-        case Division::ASSASSIN: std::cout << "Assasin"; break;
-        case Division::MAGE: std::cout << "Mage"; break;
-        case Division::MEDIC: std::cout << "Medic"; break;
-        case Division::TANK: std::cout << "Tank"; break;
+        case Division::ASSASSIN: std::cout << "Assasin" << std::endl; break;
+        case Division::MAGE: std::cout << "Mage" << std::endl; break;
+        case Division::MEDIC: std::cout << "Medic" << std::endl; break;
+        case Division::TANK: std::cout << "Tank" << std::endl; break;
+    }
+
+    std::cout << "Combat: ";
+
+    switch (hero.combat)
+    {
+        case Combat::RANGE: std::cout << "Range" << std::endl; break;
+        case Combat::MELEE: std::cout << "Mele" << std::endl; break;
+        case Combat::BOTH: std::cout << "Both" << std::endl; break;
     }
 }
 
